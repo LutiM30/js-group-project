f948e8615549b12a0e2022c3daf4bf69f0ef3170
Mitul Mistry <mitmistry311@gmail.com>
2024-03-10 16:16:46 -0400

    Code formatting and commenting

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Consts
 const demoTitle =
   "Lorem ipsum dolor sit amet, incididunt adipisicing pariatur enim.";
 const demoDesc =
@@ -7,10 +8,12 @@ const localStorageTasks = "tasks";
 
 const hide = " hidden";
 
+// localStorage data
 const localStorageTasksArr = JSON.parse(
   localStorage?.getItem(localStorageTasks)
 );
 
+// If there's no entity in the browser of tasks it'll automaticallyy create demo task
 const sampleArr = [
   {
     title: demoTitle,
@@ -22,6 +25,7 @@ const tasks = Array.isArray(localStorageTasksArr)
   ? localStorageTasksArr
   : sampleArr;
 
+// HTML for task card
 const taskCardHTML = (title = demoTitle, desc = demoDesc) =>
   `
 <div class="flex justify-between items-center">
@@ -41,17 +45,24 @@ const taskCardHTML = (title = demoTitle, desc = demoDesc) =>
 </div>
 `;
 
+// Error to show
 const setError = (errorMessage = "") => $("#error").html(errorMessage);
 
+// it contains some things which contains are additional things of the project like shortcut keys etc.
 const additionalFunctionality = {
   keybindings: {
     createTask: (e) =>
       e.ctrlKey && e.keyCode === 13 ? TasksCRUD.Create() : "",
+
     focusTask: (e) =>
       e.ctrlKey && e?.keyCode === 75 ? $("#taskTitle").focus() : "",
+
     focusSearch: (e) =>
       e.ctrlKey && e.keyCode === 191 ? $("#taskSearch").focus() : "",
   },
+
+  // Search Input contains placeholders whihch changes every two seconds
+  //  it shows exsiting Task's title randomly
   changePlaceHolder: () => {
     const tasksTitle = tasks?.map((task) => task.title);
     const randomIndex = Math.floor(Math.random() * tasksTitle.length);
@@ -61,15 +72,20 @@ const additionalFunctionality = {
       `Ctrl + / for search (${tasksTitle[randomIndex] || "No Tasks right now"})`
     );
   },
+
+  //  When Editing the Task if window gets changed it'll store the task if title and description are there.
   lostFocus: () =>
     $("#taskTitle").val() && $("#taskDescription").val() && TasksCRUD.Create(),
-  toggleDropdown: () => {
-    $("#dropdownDefaultCheckbox").attr("class", (i, origValue) =>
+
+  // open the dropdown by passing the id of the dropdown
+  toggleDropdown: (dropdownId) => () => {
+    $(`#${dropdownId}`).attr("class", (i, origValue) =>
       origValue.includes(hide) ? origValue.replace(hide, "") : origValue + hide
     );
   },
 };
 
+// Things Related to task
 const TasksCRUD = {
   Create: () => {
     const title = $("#taskTitle").val();
@@ -90,6 +106,7 @@ const TasksCRUD = {
       setError("Title and Description is Required");
     }
   },
+
   Read: () => {
     const taskList = document.getElementById("taskList");
     taskList.innerHTML = ""; // Clear existing tasks
@@ -122,12 +139,14 @@ const TasksCRUD = {
 
     localStorage.setItem(localStorageTasks, JSON.stringify(tasks));
   },
+
   Update: (index) => {
     $("#taskTitle").val(tasks[index].title);
     $("#taskDescription").val(tasks[index].description);
     $("#taskTitle").focus();
     TasksCRUD.Delete(index);
   },
+
   Delete: (index) => {
     tasks.splice(index, 1); // Remove the task at the specified index
     TasksCRUD.Read();
@@ -136,7 +155,9 @@ const TasksCRUD = {
 
 $(document).ready(() => {
   $("#addTask").click(TasksCRUD.Create);
-  $("#dropdownCheckboxButton").click(additionalFunctionality.toggleDropdown);
+  $("#assigneeButton").click(
+    additionalFunctionality.toggleDropdown("assigneeList")
+  );
 
   $(window).blur(additionalFunctionality.lostFocus);
 
@@ -154,9 +175,3 @@ $(document).ready(() => {
   // initial rendering
   TasksCRUD.Read();
 });
-
-// Initialize with existing tasks
-const deleteTask = (index) => {
-  tasks.splice(index, 1); // Remove the task at the specified index
-  TasksCRUD.Read();
-};
